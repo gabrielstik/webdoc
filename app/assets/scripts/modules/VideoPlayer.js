12ac04d7818f0dfd6a1706de59a9e058e7c49fb4
vtimsit <victor.timsit@hetic.net>
2018-06-26 14:01:49 +0200

    :sparkles: VideoPlayer

--- app/assets/scripts/modules/VideoPlayer.js
+++ app/assets/scripts/modules/VideoPlayer.js
@@ -6,53 +6,169 @@ export default class VideoPlayer
    * @param {path} videoUrl - path to load video
    */
   
-  constructor(wrapperClass, videoUrl)
+  constructor(wrapperClass, videoUrl, videoTitle)
   {
     // Select container
     const wrapper = document.querySelector('.' + wrapperClass)
 
     // Create DOM element & class & attributes
-    const videoContainer = document.createElement('div')
-    videoContainer.classList.add('videoPlayer')
+    this.videoContainer = document.createElement('div')
+    this.videoContainer.classList.add('videoPlayer')
 
+		// Set class & attribute of video
     this.video = document.createElement('video')
-    this.video.setAttribute('src', videoUrl)
+		this.video.classList.add('videoPlayer__video')
+		this.video.setAttribute('src', videoUrl)
+
+		// Set timeline & timelineFill
+		this.timeline = document.createElement('div')
+		this.timelineFill = document.createElement('div')
+
+		// Set play icon
+		this.playIcon = document.createElement('div')
+		this.playIcon.classList.add('videoPlayer__playIcon')
+
+		// Set pause icon
+		this.pauseIcon = 
+		{
+			container: document.createElement('div'),
+			barLeft: document.createElement('div'),
+			barRight: document.createElement('div'),
+		}
+
+		this.gradient = document.createElement('div')
+		this.videoTitle = document.createElement('h3')
+
+		// Set pause icon elements class
+		this.pauseIcon.container.classList.add('videoPlayer__pauseIcon')
+		this.pauseIcon.barLeft.classList.add('videoPlayer__pauseBar')
+		this.pauseIcon.barRight.classList.add('videoPlayer__pauseBar')
+
+		this.gradient.classList.add('videoPlayer__gradient')
+		this.videoTitle.classList.add('videoPlayer__title')
+
+		this.videoTitle.innerHTML = videoTitle
+
+		// Push pause bar into pause icon
+		this.pauseIcon.container.appendChild(this.pauseIcon.barLeft)
+		this.pauseIcon.container.appendChild(this.pauseIcon.barRight)
+
+		this.video.addEventListener('loadedmetadata', () => {
+			this.videoDuration = this.video.duration
+
+			// Init methods
+			this.init()
+		})
 
     // Inject in container
-    videoContainer.appendChild(this.video)
-    wrapper.appendChild(videoContainer)
+		this.videoContainer.appendChild(this.video)
+		this.videoContainer.appendChild(this.gradient)
+		this.videoContainer.appendChild(this.videoTitle)
+    this.videoContainer.appendChild(this.playIcon)
+		this.videoContainer.appendChild(this.pauseIcon.container)
+		
+		wrapper.appendChild(this.videoContainer)
 
     // Click event to play()
-    document.addEventListener('click', () => { this.video.play() }, { once : true })
+		document.addEventListener('click', () => { this.video.play() }, { once : true })
 
-    // Init method
-    this.init(videoContainer)
+		// Play or pause video when click on video
+		this.video.addEventListener('click', () => { this.video.paused ? this.playVideo() : this.pauseVideo() })
+
+		// Listen timeline events to update currentTime and timeline style
+		this.timeline.addEventListener('mousedown', () => 
+		{ 
+			// State of mouseup
+			let mouseUp = false
+
+			// Mouse move on window
+			window.addEventListener('mousemove', (event) => 
+			{
+				// Update current time on mouseup
+				if(!mouseUp) { this.updateCurrentTime(event) }
+
+			})
+			// Mouse set at true when mouseup on window
+			window.addEventListener('mouseup', () => 
+			{
+				mouseUp = true
+			})
+		})
+
+		// Update current time of video
+		this.timeline.addEventListener('click', (event) => 
+		{
+			this.updateCurrentTime(event)
+		})
   }
 
-  init(videoContainer)
+  init()
   {
     // Create timeline DOM
-    const timeline = document.createElement('div')
-    timeline.classList.add('videoPlayer__timeline')
+    this.timeline.classList.add('videoPlayer__timeline')
 
     // Create timelineFill DOM
-    const timelineFill = document.createElement('div')
-    timelineFill.classList.add('videoPlayer__timelineFill')
+    this.timelineFill.classList.add('videoPlayer__timelineFill')
 
     // AppendChild timeline into videoContainer
-    timeline.appendChild(timelineFill)
-    videoContainer.appendChild(timeline)
-  }
+    this.timeline.appendChild(this.timelineFill)
+		this.videoContainer.appendChild(this.timeline)
+		
+		// Update timeline style
+		this.updateTimeline()
+	}
+	
+	updateTimeline()
+	{
+		// Set animation frame
+		const animationFrame = window.requestAnimationFrame(this.updateTimeline.bind(this, this.timelineFill))
 
-  play()
+		// Set scaleX of timeline
+		this.timelineFill.style.transform = `scaleX(${this.video.currentTime / this.videoDuration})`
+
+		// Cancel aniamtionFrame when video in ending
+		if(this.video.currentTime == this.videoDuration)
+		{ 
+			this.resetVideo()
+			this.resetTimeline()
+			this.playVideo()
+			this.init()
+			window.cancelAnimationFrame(animationFrame) 
+		}
+	}
+
+	updateCurrentTime(event)
+	{
+		this.video.currentTime = event.clientX / this.timeline.offsetWidth * this.video.duration
+	}
+
+  playVideo()
   {
+		this.playIcon.style.opacity = '1'
+		setTimeout(() => { this.playIcon.style.opacity = '0' }, 200)
+
     // play video
-    this.video.play()
+		this.video.play()
+		
   }
   
-  pause()
+  pauseVideo()
   {
+		this.pauseIcon.container.style.opacity = '1'
+		setTimeout(() => { this.pauseIcon.container.style.opacity = '0' }, 200)
+
     // pause video
-    this.video.pause()
-  }
+		this.video.pause()
+
+	}
+
+	resetVideo()
+	{
+		this.video.currentTime = 0
+	}
+
+	resetTimeline()
+	{
+		this.timelineFill.style.transform = `scaleX(0)`
+	}
 }
