cbc6280626c61e45d860378ec599e889d29ead10
vtimsit <victor.timsit@hetic.net>
2018-06-27 15:12:44 +0200

    :construction: ScrollBar

--- app/assets/scripts/controllers/DevController.js
+++ app/assets/scripts/controllers/DevController.js
@@ -1,11 +1,13 @@
 import GraphCanvas from '../modules/GraphCanvas'
 import Parallax from '../modules/Parallax';
+import ScrollBar from '../modules/ScrollBar'
 
 export default class DevController
 {
     init()
     {
-        new GraphCanvas('dev', 'assets/images/favelas/wall.jpeg', 'assets/images/favelas/wark.png', 'assets/images/favelas/warkBlend.png')
-        new Parallax('graph__wrapper', 1, true)
+        // new GraphCanvas('dev', 'assets/images/favelas/wall.jpeg', 'assets/images/favelas/wark.png', 'assets/images/favelas/warkBlend.png')
+        // new Parallax('graph__wrapper', 1, true)
+        new ScrollBar(7, 'dev')
     }
 }
