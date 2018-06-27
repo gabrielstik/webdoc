298a6766f9f298932a50eeaf9c23890d242640e2
vtimsit <victor.timsit@hetic.net>
2018-06-27 20:49:53 +0200

    :art: Fixed style of scroll bar

--- app/assets/scripts/controllers/DevController.js
+++ app/assets/scripts/controllers/DevController.js
@@ -8,6 +8,6 @@ export default class DevController
     {
         // new GraphCanvas('dev', 'assets/images/favelas/wall.jpeg', 'assets/images/favelas/wark.png', 'assets/images/favelas/warkBlend.png')
         // new Parallax('graph__wrapper', 1, true)
-        new ScrollBar(7, 'dev')
+        new ScrollBar(7, 'dev', '80', '15')
     }
 }
